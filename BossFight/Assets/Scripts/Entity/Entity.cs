4d3f44189faf4de134e509848d059c1e4a66beed
Arqu3 <ludwiggustavsson3@gmail.com>
2016-11-21 21:04:20 +0100

    Added entity base class

---
+++ BossFight/Assets/Scripts/Entity/Entity.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Entity : MonoBehaviour
+{
+    //Public vars
+    Rigidbody m_Rigidbody;
+
+	public virtual void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	void Update()
+    {
+	}
+
+    public virtual void Move()
+    {
+    }
+
+    public virtual void Attack()
+    {
+    }
+}
